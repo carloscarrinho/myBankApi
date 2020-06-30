ae3ec0621b4a73fae0818316cb2e37b165ae924d
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-06-30 17:20:50 -0300

    Config database (mongodb) connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import mongo from '../config/mongo.js';
+
+dotenv.config();
 
 try {
-  mongoose.connect();
+  mongoose.connect(`mongodb://${mongo.host}:${mongo.port}/${mongo.database}`, mongo.options);
   console.log("MongoDB Connected");
 
 } catch (err) {
   console.log(err);
-  
 }
+
+export default mongoose;
